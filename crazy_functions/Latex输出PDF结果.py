af3a1901a0837290363747897d4c974b7c7cf162
kaixindelele <1985790413@qq.com>
2023-06-20 21:06:13 +0800

    Update Latex输出PDF结果.py

--- crazy_functions/Latex输出PDF结果.py
+++ crazy_functions/Latex输出PDF结果.py
@@ -3,8 +3,8 @@ from toolbox import CatchException, report_execption, update_ui_lastest_msg, zip
 import glob, os, requests, time
 pj = os.path.join
 # ARXIV_CACHE_DIR = os.path.expanduser(f"~/arxiv_cache/")
+# ARXIV_CACHE_DIR = os.path.expanduser(f"~/arxiv_cache/")
 ARXIV_CACHE_DIR = os.getenv("Arxiv_Cache")
-
 # =================================== 工具函数 ===============================================
 专业词汇声明  = 'If the term "agent" is used in this section, it should be translated to "智能体". '
 def switch_prompt(pfg, mode):
